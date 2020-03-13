6a711220cf9f9304a404e989e98f04773e6770ef
seayounHan <helious4me@gmail.com>
2020-03-13 17:57:20 +0900

    [20.03.13] first commit

---
+++ android/app/src/main/kotlin/teamswanproject20200313/team_swan_project_20200313/MainActivity.kt
@@ -0,0 +1,13 @@
+package teamswanproject20200313.team_swan_project_20200313
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
